7f72d4e6dc8ce5e4bd6126ca9dce127cf5689ff2
kingj1994 <ga394396049@163.com>
2019-10-28 15:21:19 +0800

    add changes.

--- util/bloom/bloomfilter.go
+++ util/bloom/bloomfilter.go
@@ -5,34 +5,35 @@ import (
 	"hash/fnv"
 )
 
-// BloomFilter a simple abstraction of bloom filter
-type BloomFilter struct {
+// Filter a simple abstraction of bloom filter
+type Filter struct {
 	bitSet   []uint64
 	length   uint64
 	unitSize uint64
 }
 
-func NewBloomFilter(length int) (*BloomFilter, error) {
+// NewFilter return a bloom filter with a empty bitset of given length
+func NewFilter(length int) (*Filter, error) {
 	if length <= 0 {
 		return nil, fmt.Errorf("length is not positive")
 	}
 	bitset := make([]uint64, length)
 	bits := uint64(64)
-	return &BloomFilter{
+	return &Filter{
 		bitSet:   bitset,
 		length:   bits * uint64(length),
 		unitSize: bits,
 	}, nil
 }
 
-// NewBloomFilterBySlice create a bloom filter by the given slice
-func NewBloomFilterBySlice(bs []uint64) (*BloomFilter, error) {
+// NewFilterBySlice create a bloom filter by the given slice
+func NewFilterBySlice(bs []uint64) (*Filter, error) {
 	if len(bs) == 0 {
 		return nil, fmt.Errorf("len(bs) == 0")
 	}
 
 	bits := uint64(64)
-	return &BloomFilter{
+	return &Filter{
 		bitSet:   bs,
 		length:   bits * uint64(len(bs)),
 		unitSize: bits,
@@ -40,19 +41,19 @@ func NewBloomFilterBySlice(bs []uint64) (*BloomFilter, error) {
 }
 
 // Insert a key into the filter
-func (bf *BloomFilter) Insert(key []byte) {
+func (bf *Filter) Insert(key []byte) {
 	idx, shift := bf.hash(key)
 	bf.bitSet[idx] |= 1 << shift
 }
 
 // Probe check whether the given key is in the filter
-func (bf *BloomFilter) Probe(key []byte) bool {
+func (bf *Filter) Probe(key []byte) bool {
 	idx, shift := bf.hash(key)
 
 	return bf.bitSet[idx]&(1<<shift) != 0
 }
 
-func (bf *BloomFilter) hash(key []byte) (uint64, uint64) {
+func (bf *Filter) hash(key []byte) (uint64, uint64) {
 	hash := ihash(key) % uint64(bf.length)
 	idx := hash / bf.unitSize
 	shift := hash % bf.unitSize
